01543df1a98a71d6f3d44a1863f51b49719cd640
Jack <somedd@naver.com>
2018-01-10 11:14:27 +0900

    test

--- CardGame/CardGame/main.swift
+++ CardGame/CardGame/main.swift
@@ -21,7 +21,7 @@ while runCardGame {
     
 // 잠시 기능을 일시적으로 제거 하기 위해서 일부 기능만(제 코드에서는 inputview + switch구문) 주석을 처리하는 것이 가장 좋다고 생각합니다.
 // (고민이 부족한 건가요?ㅠㅠ)
-    
+//    
 //    outputView.printMessage(.inputMessage)
 //    let userMenu = InputView().readMenu()
 //    switch userMenu {
